e72e29069b89ca3951dba472360902ed8621a3ac
Cleydson Luz <cleydson.luz@procenge.com.br>
2020-11-19 00:07:20 -0300

    Alteração no legends never Die

--- public/bundle.js
+++ public/bundle.js
@@ -1,5 +1,6 @@
 /*
-* Legends Never Die BUT THEY DIE
+* Rubinho 
+* Legends Never Die BUT THEY DIE and DIEEE
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is not neither made for production nor for readable output files.
  * It uses "eval()" calls to create a separate source file in the browser devtools.
