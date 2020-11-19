6222d716ca394e2c08b96757d5dbae76b539f835
Cleydson Luz <cleydson.luz@procenge.com.br>
2020-11-19 00:05:55 -0300

    Adição Legends Nerver Die

--- public/bundle.js
+++ public/bundle.js
@@ -1,5 +1,5 @@
 /*
-* Legends Never Die
+* Legends Never Die BUT THEY DIE
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is not neither made for production nor for readable output files.
  * It uses "eval()" calls to create a separate source file in the browser devtools.
