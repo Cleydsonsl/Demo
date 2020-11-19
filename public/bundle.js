c37d6c0664f99efaa65d3b1d1e37808935d3d825
Cleydson Luz <cleydson.luz@procenge.com.br>
2020-11-19 00:11:29 -0300

    Pull alteracoes

--- public/bundle.js
+++ public/bundle.js
@@ -1,6 +1,7 @@
 /*
 * Rubinho 
-* Legends Never Die BUT THEY DIE and DIEEE
+* Legends Never Die BUT THEY DIE and DIEEE´
+* OLOKITO MEO
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is not neither made for production nor for readable output files.
  * It uses "eval()" calls to create a separate source file in the browser devtools.
